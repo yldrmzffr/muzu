5af4c201137faf834f38c83bc71b8fa1a6e27c00
yldrmzffr <mail@muzaffer.dev>
2023-11-12 10:11:47 +0300

    feat(major restructuring): implement controller and request handler classes, refactor routing
    This update involves implementing new controller and request handler classes, along with refactoring
    the routing system. The new controllers are responsible for managing distinct functionalities of the
    application, enhancing modularity and clarity in the codebase. Request handlers are designed to
    process incoming HTTP requests, ensuring they're directed to the appropriate controllers, while
    handling validation and error management. The refactoring of the routing system aligns with these
    new structures, improving the efficiency and clarity of URL routing and request processing. Overall,
    these changes aim to make the application more robust, maintainable, and scalable, facilitating
    easier updates and expansions in the future.

---
+++ lib/methods/method-factory.ts
@@ -0,0 +1,46 @@
+import {RequestMethod} from '../types';
+
+type HttpMethodDecorator = (
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) => PropertyDescriptor;
+
+export type MethodDecorator = (url?: string) => HttpMethodDecorator;
+
+export class MethodFactory {
+  public HttpMethod(method: RequestMethod, url: string): HttpMethodDecorator {
+    return (
+      target: any,
+      propertyKey: string,
+      descriptor: PropertyDescriptor
+    ) => {
+      Reflect.defineMetadata(
+        'url',
+        `${url}`.toLowerCase(),
+        target[propertyKey]
+      );
+      Reflect.defineMetadata('method', method, target[propertyKey]);
+      return descriptor;
+    };
+  }
+  Get = (url = '/'): HttpMethodDecorator => {
+    return this.HttpMethod(RequestMethod.GET, url);
+  };
+
+  Post = (url = '/'): HttpMethodDecorator => {
+    return this.HttpMethod(RequestMethod.POST, url);
+  };
+
+  Delete = (url = '/'): HttpMethodDecorator => {
+    return this.HttpMethod(RequestMethod.DELETE, url);
+  };
+
+  Put = (url = '/'): HttpMethodDecorator => {
+    return this.HttpMethod(RequestMethod.PUT, url);
+  };
+
+  Patch = (url = '/'): HttpMethodDecorator => {
+    return this.HttpMethod(RequestMethod.PATCH, url);
+  };
+}
